4251ac7b21a7c214a8ee90f10c984f36fdbe3cc7
JeanLeFlambeur <catalin.vasile@gmail.com>
2015-07-16 15:42:09 +0200

    the epoch is now the start of the program for all platforms

--- qbase/src/Clock.cpp
+++ qbase/src/Clock.cpp
@@ -17,42 +17,48 @@
 #	endif
 #endif
 
-using namespace q;
+namespace q
+{
 
-Clock::time_point Clock::now()
+static Clock::rep get_now()
 {
 #if defined(Q_WINDOWS)
 
-	LARGE_INTEGER freq;
-	LARGE_INTEGER crt;
-	QueryPerformanceFrequency(&freq);
-	QueryPerformanceCounter(&crt);
-	return time_point(duration(crt.QuadPart * static_cast<rep>(period::den) / freq.QuadPart));
+    LARGE_INTEGER freq;
+    LARGE_INTEGER crt;
+    QueryPerformanceFrequency(&freq);
+    QueryPerformanceCounter(&crt);
+    return Clock::rep(crt.QuadPart * static_cast<Clock::rep>(Clock::period::den) / freq.QuadPart);
 
 #elif defined(Q_POSIX_API)
 
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
-
-    return time_point(duration(ts.tv_nsec + ts.tv_sec * static_cast<rep>(period::den)));
-
-//	struct timeval ptv;
-//    gettimeofday(&ptv, nullptr);
-//	return time_point(duration(ptv.tv_usec + ptv.tv_sec * static_cast<rep>(period::den)));
+    return Clock::rep(ts.tv_nsec + ts.tv_sec * static_cast<Clock::rep>(Clock::period::den));
 
 #else
 #	error "Define for your platform"
 #endif
 }
 
+static Clock::rep s_start_time_point = get_now();
+
+
+Clock::time_point Clock::now()
+{
+    Clock::rep now = get_now();
+    return time_point(duration(now - s_start_time_point));
+}
+
 time_t Clock::to_time_t(time_point tp)
 {
-	time_t t = tp.time_since_epoch().count();
+    time_t t = std::chrono::duration_cast<std::chrono::seconds>(tp.time_since_epoch()).count();
 	return t;
 
 }
 Clock::time_point Clock::from_time_t(time_t t)
 {
-	return time_point(duration(t));
+    return time_point(std::chrono::duration_cast<duration>(std::chrono::seconds(t)));
 }
 
+}
