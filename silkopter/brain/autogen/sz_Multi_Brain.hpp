ce12125cdf979c221e3575495891d42fab038827
jean <catalin.vasile@gmail.com>
2015-08-22 18:05:46 +0200

    added Horizontal PIDs

--- silkopter/brain/autogen/sz_Multi_Brain.hpp
+++ silkopter/brain/autogen/sz_Multi_Brain.hpp
@@ -456,10 +456,592 @@ w.Key("\x53\x74\x61\x74\x65\x20\x52\x61\x74\x65\x20\x28\x48\x7a\x29", 15, false)
 // The comments are reserved for replacement
 // such syntax is chosen so that the template file looks like valid C++
 
-namespace sz { namespace Multi_Brain { struct Config {
- 
+namespace sz { namespace Multi_Brain { struct Horizontal_Angle {
+ float max_speed_deg;
+bool combined_pids;
+sz::PID pids;
+sz::PID x_pid;
+sz::PID y_pid;
 
-explicit Config() {  }
+explicit Horizontal_Angle():max_speed_deg(180), combined_pids(true), pids(), x_pid(), y_pid() {  }
+
+
+ 
+}; }
+ }
+
+
+namespace autojsoncxx {
+
+template <>
+class SAXEventHandler< ::sz::Multi_Brain::Horizontal_Angle > {
+private:
+    utility::scoped_ptr<error::ErrorBase> the_error;
+    int state;
+    int depth;
+
+    SAXEventHandler< float > handler_0;
+SAXEventHandler< bool > handler_1;
+SAXEventHandler< sz::PID > handler_2;
+SAXEventHandler< sz::PID > handler_3;
+SAXEventHandler< sz::PID > handler_4;bool has_max_speed_deg;
+bool has_combined_pids;
+
+    bool check_depth(const char* type)
+    {
+        if (depth <= 0) {
+            the_error.reset(new error::TypeMismatchError("object", type));
+            return false;
+        }
+        return true;
+    }
+
+    const char* current_member_name() const
+    {
+        switch (state) {
+            case 0:
+    return "max_speed_deg";
+case 1:
+    return "combined_pids";
+case 2:
+    return "pids";
+case 3:
+    return "x_pid";
+case 4:
+    return "y_pid";
+        default:
+            break;
+        }
+        return "<UNKNOWN>";
+    }
+
+    bool checked_event_forwarding(bool success)
+    {
+        if (!success)
+            the_error.reset(new error::ObjectMemberError(current_member_name()));
+        return success;
+    }
+
+    void set_missing_required(const char* name)
+    {
+        if (the_error.empty() || the_error->type() != error::MISSING_REQUIRED)
+            the_error.reset(new error::RequiredFieldMissingError());
+
+        std::vector<std::string>& missing =
+            static_cast<error::RequiredFieldMissingError*>(the_error.get())->missing_members();
+
+        missing.push_back(name);
+    }
+
+    void reset_flags()
+    {
+        has_max_speed_deg = false;
+has_combined_pids = false;
+
+
+
+    }
+
+public:
+    explicit SAXEventHandler( ::sz::Multi_Brain::Horizontal_Angle * obj)
+        : state(-1)
+        , depth(0)
+        , handler_0(&obj->max_speed_deg)
+, handler_1(&obj->combined_pids)
+, handler_2(&obj->pids)
+, handler_3(&obj->x_pid)
+, handler_4(&obj->y_pid)
+    {
+        reset_flags();
+    }
+
+    bool Null()
+    {
+        if (!check_depth("null"))
+            return false;
+
+        switch (state) {
+
+        case 0:
+    return checked_event_forwarding(handler_0.Null());
+
+case 1:
+    return checked_event_forwarding(handler_1.Null());
+
+case 2:
+    return checked_event_forwarding(handler_2.Null());
+
+case 3:
+    return checked_event_forwarding(handler_3.Null());
+
+case 4:
+    return checked_event_forwarding(handler_4.Null());
+
+        default:
+            break;
+        }
+        return true;
+    }
+
+    bool Bool(bool b)
+    {
+        if (!check_depth("bool"))
+            return false;
+
+        switch (state) {
+
+        case 0:
+    return checked_event_forwarding(handler_0.Bool(b));
+
+case 1:
+    return checked_event_forwarding(handler_1.Bool(b));
+
+case 2:
+    return checked_event_forwarding(handler_2.Bool(b));
+
+case 3:
+    return checked_event_forwarding(handler_3.Bool(b));
+
+case 4:
+    return checked_event_forwarding(handler_4.Bool(b));
+
+        default:
+            break;
+        }
+        return true;
+    }
+
+    bool Int(int i)
+    {
+        if (!check_depth("int"))
+            return false;
+
+        switch (state) {
+
+        case 0:
+    return checked_event_forwarding(handler_0.Int(i));
+
+case 1:
+    return checked_event_forwarding(handler_1.Int(i));
+
+case 2:
+    return checked_event_forwarding(handler_2.Int(i));
+
+case 3:
+    return checked_event_forwarding(handler_3.Int(i));
+
+case 4:
+    return checked_event_forwarding(handler_4.Int(i));
+
+        default:
+            break;
+        }
+        return true;
+    }
+
+    bool Uint(unsigned i)
+    {
+        if (!check_depth("unsigned"))
+            return false;
+
+        switch (state) {
+
+        case 0:
+    return checked_event_forwarding(handler_0.Uint(i));
+
+case 1:
+    return checked_event_forwarding(handler_1.Uint(i));
+
+case 2:
+    return checked_event_forwarding(handler_2.Uint(i));
+
+case 3:
+    return checked_event_forwarding(handler_3.Uint(i));
+
+case 4:
+    return checked_event_forwarding(handler_4.Uint(i));
+
+        default:
+            break;
+        }
+        return true;
+    }
+
+    bool Int64(utility::int64_t i)
+    {
+        if (!check_depth("int64_t"))
+            return false;
+
+        switch (state) {
+
+        case 0:
+    return checked_event_forwarding(handler_0.Int64(i));
+
+case 1:
+    return checked_event_forwarding(handler_1.Int64(i));
+
+case 2:
+    return checked_event_forwarding(handler_2.Int64(i));
+
+case 3:
+    return checked_event_forwarding(handler_3.Int64(i));
+
+case 4:
+    return checked_event_forwarding(handler_4.Int64(i));
+
+        default:
+            break;
+        }
+        return true;
+    }
+
+    bool Uint64(utility::uint64_t i)
+    {
+        if (!check_depth("uint64_t"))
+            return false;
+
+        switch (state) {
+
+        case 0:
+    return checked_event_forwarding(handler_0.Uint64(i));
+
+case 1:
+    return checked_event_forwarding(handler_1.Uint64(i));
+
+case 2:
+    return checked_event_forwarding(handler_2.Uint64(i));
+
+case 3:
+    return checked_event_forwarding(handler_3.Uint64(i));
+
+case 4:
+    return checked_event_forwarding(handler_4.Uint64(i));
+
+        default:
+            break;
+        }
+        return true;
+    }
+
+    bool Double(double d)
+    {
+        if (!check_depth("double"))
+            return false;
+
+        switch (state) {
+
+        case 0:
+    return checked_event_forwarding(handler_0.Double(d));
+
+case 1:
+    return checked_event_forwarding(handler_1.Double(d));
+
+case 2:
+    return checked_event_forwarding(handler_2.Double(d));
+
+case 3:
+    return checked_event_forwarding(handler_3.Double(d));
+
+case 4:
+    return checked_event_forwarding(handler_4.Double(d));
+
+        default:
+            break;
+        }
+        return true;
+    }
+
+    bool String(const char* str, SizeType length, bool copy)
+    {
+        if (!check_depth("string"))
+            return false;
+
+        switch (state) {
+
+        case 0:
+    return checked_event_forwarding(handler_0.String(str, length, copy));
+
+case 1:
+    return checked_event_forwarding(handler_1.String(str, length, copy));
+
+case 2:
+    return checked_event_forwarding(handler_2.String(str, length, copy));
+
+case 3:
+    return checked_event_forwarding(handler_3.String(str, length, copy));
+
+case 4:
+    return checked_event_forwarding(handler_4.String(str, length, copy));
+
+        default:
+            break;
+        }
+        return true;
+    }
+
+    bool Key(const char* str, SizeType length, bool copy)
+    {
+        if (!check_depth("object"))
+            return false;
+
+        if (depth == 1) {
+            if (0) {
+            }
+            else if (utility::string_equal(str, length, "\x4d\x61\x78\x20\x53\x70\x65\x65\x64\x20\x28\x64\x65\x67\x2f\x73\x29", 17))
+						 { state=0; has_max_speed_deg = true; }
+else if (utility::string_equal(str, length, "\x43\x6f\x6d\x62\x69\x6e\x65\x64\x20\x50\x49\x44\x73", 13))
+						 { state=1; has_combined_pids = true; }
+else if (utility::string_equal(str, length, "\x50\x49\x44\x73", 4))
+						 { state=2;  }
+else if (utility::string_equal(str, length, "\x58\x20\x50\x49\x44", 5))
+						 { state=3;  }
+else if (utility::string_equal(str, length, "\x59\x20\x50\x49\x44", 5))
+						 { state=4;  }
+            else {
+                state = -1;
+                return true;
+            }
+
+        } else {
+            switch (state) {
+
+            case 0:
+    return checked_event_forwarding(handler_0.Key(str, length, copy));
+
+case 1:
+    return checked_event_forwarding(handler_1.Key(str, length, copy));
+
+case 2:
+    return checked_event_forwarding(handler_2.Key(str, length, copy));
+
+case 3:
+    return checked_event_forwarding(handler_3.Key(str, length, copy));
+
+case 4:
+    return checked_event_forwarding(handler_4.Key(str, length, copy));
+
+            default:
+                break;
+            }
+        }
+        return true;
+    }
+
+    bool StartArray()
+    {
+        if (!check_depth("array"))
+            return false;
+
+        switch (state) {
+
+        case 0:
+    return checked_event_forwarding(handler_0.StartArray());
+
+case 1:
+    return checked_event_forwarding(handler_1.StartArray());
+
+case 2:
+    return checked_event_forwarding(handler_2.StartArray());
+
+case 3:
+    return checked_event_forwarding(handler_3.StartArray());
+
+case 4:
+    return checked_event_forwarding(handler_4.StartArray());
+
+        default:
+            break;
+        }
+        return true;
+    }
+
+    bool EndArray(SizeType length)
+    {
+        if (!check_depth("array"))
+            return false;
+
+        switch (state) {
+
+        case 0:
+    return checked_event_forwarding(handler_0.EndArray(length));
+
+case 1:
+    return checked_event_forwarding(handler_1.EndArray(length));
+
+case 2:
+    return checked_event_forwarding(handler_2.EndArray(length));
+
+case 3:
+    return checked_event_forwarding(handler_3.EndArray(length));
+
+case 4:
+    return checked_event_forwarding(handler_4.EndArray(length));
+
+        default:
+            break;
+        }
+        return true;
+    }
+
+    bool StartObject()
+    {
+        ++depth;
+        if (depth > 1) {
+
+            switch (state) {
+
+            case 0:
+    return checked_event_forwarding(handler_0.StartObject());
+
+case 1:
+    return checked_event_forwarding(handler_1.StartObject());
+
+case 2:
+    return checked_event_forwarding(handler_2.StartObject());
+
+case 3:
+    return checked_event_forwarding(handler_3.StartObject());
+
+case 4:
+    return checked_event_forwarding(handler_4.StartObject());
+
+            default:
+                break;
+            }
+        }
+        return true;
+    }
+
+    bool EndObject(SizeType length)
+    {
+        --depth;
+        if (depth > 0) {
+
+            switch (state) {
+
+            case 0:
+    return checked_event_forwarding(handler_0.EndObject(length));
+
+case 1:
+    return checked_event_forwarding(handler_1.EndObject(length));
+
+case 2:
+    return checked_event_forwarding(handler_2.EndObject(length));
+
+case 3:
+    return checked_event_forwarding(handler_3.EndObject(length));
+
+case 4:
+    return checked_event_forwarding(handler_4.EndObject(length));
+
+            default:
+                break;
+            }
+        } else {
+            if (!has_max_speed_deg) set_missing_required("max_speed_deg");
+if (!has_combined_pids) set_missing_required("combined_pids");
+        }
+        return the_error.empty();
+    }
+
+    bool HasError() const
+    {
+        return !this->the_error.empty();
+    }
+
+    bool ReapError(error::ErrorStack& errs)
+    {
+        if (this->the_error.empty())
+            return false;
+
+        errs.push(this->the_error.release());
+
+        switch (state) {
+
+        case 0:
+     handler_0.ReapError(errs); break;
+case 1:
+     handler_1.ReapError(errs); break;
+case 2:
+     handler_2.ReapError(errs); break;
+case 3:
+     handler_3.ReapError(errs); break;
+case 4:
+     handler_4.ReapError(errs); break;
+
+        default:
+            break;
+        }
+
+        return true;
+    }
+
+    void PrepareForReuse()
+    {
+        depth = 0;
+        state = -1;
+        the_error.reset();
+        reset_flags();
+        handler_0.PrepareForReuse();
+handler_1.PrepareForReuse();
+handler_2.PrepareForReuse();
+handler_3.PrepareForReuse();
+handler_4.PrepareForReuse();
+
+    }
+};
+
+template < class Writer156820147a455f68828cd15d23350dc9882f3ec8729abe53f154ed3172f93cec >
+struct Serializer< Writer156820147a455f68828cd15d23350dc9882f3ec8729abe53f154ed3172f93cec, ::sz::Multi_Brain::Horizontal_Angle > {
+
+    void operator()( Writer156820147a455f68828cd15d23350dc9882f3ec8729abe53f154ed3172f93cec& w, const ::sz::Multi_Brain::Horizontal_Angle& value) const
+    {
+        w.StartObject();
+
+        w.Key("\x4d\x61\x78\x20\x53\x70\x65\x65\x64\x20\x28\x64\x65\x67\x2f\x73\x29", 17, false); Serializer< Writer156820147a455f68828cd15d23350dc9882f3ec8729abe53f154ed3172f93cec, float >()(w, value.max_speed_deg);
+w.Key("\x43\x6f\x6d\x62\x69\x6e\x65\x64\x20\x50\x49\x44\x73", 13, false); Serializer< Writer156820147a455f68828cd15d23350dc9882f3ec8729abe53f154ed3172f93cec, bool >()(w, value.combined_pids);
+w.Key("\x50\x49\x44\x73", 4, false); Serializer< Writer156820147a455f68828cd15d23350dc9882f3ec8729abe53f154ed3172f93cec, sz::PID >()(w, value.pids);
+w.Key("\x58\x20\x50\x49\x44", 5, false); Serializer< Writer156820147a455f68828cd15d23350dc9882f3ec8729abe53f154ed3172f93cec, sz::PID >()(w, value.x_pid);
+w.Key("\x59\x20\x50\x49\x44", 5, false); Serializer< Writer156820147a455f68828cd15d23350dc9882f3ec8729abe53f154ed3172f93cec, sz::PID >()(w, value.y_pid);
+
+        w.EndObject(5);
+    }
+
+};
+}
+
+
+// The MIT License (MIT)
+//
+// Copyright (c) 2014 Siyuan Ren (netheril96@gmail.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <autojsoncxx/autojsoncxx.hpp>
+
+// The comments are reserved for replacement
+// such syntax is chosen so that the template file looks like valid C++
+
+namespace sz { namespace Multi_Brain { struct Config {
+ sz::Multi_Brain::Horizontal_Angle horizontal_angle;
+
+explicit Config():horizontal_angle() {  }
 
 
  
@@ -476,7 +1058,7 @@ private:
     int state;
     int depth;
 
-    
+    SAXEventHandler< sz::Multi_Brain::Horizontal_Angle > handler_0;
 
     bool check_depth(const char* type)
     {
@@ -490,7 +1072,8 @@ private:
     const char* current_member_name() const
     {
         switch (state) {
-            
+            case 0:
+    return "horizontal_angle";
         default:
             break;
         }
@@ -524,7 +1107,7 @@ public:
     explicit SAXEventHandler( ::sz::Multi_Brain::Config * obj)
         : state(-1)
         , depth(0)
-        
+        , handler_0(&obj->horizontal_angle)
     {
         reset_flags();
     }
@@ -536,7 +1119,8 @@ public:
 
         switch (state) {
 
-        
+        case 0:
+    return checked_event_forwarding(handler_0.Null());
 
         default:
             break;
@@ -551,7 +1135,8 @@ public:
 
         switch (state) {
 
-        
+        case 0:
+    return checked_event_forwarding(handler_0.Bool(b));
 
         default:
             break;
@@ -566,7 +1151,8 @@ public:
 
         switch (state) {
 
-        
+        case 0:
+    return checked_event_forwarding(handler_0.Int(i));
 
         default:
             break;
@@ -581,7 +1167,8 @@ public:
 
         switch (state) {
 
-        
+        case 0:
+    return checked_event_forwarding(handler_0.Uint(i));
 
         default:
             break;
@@ -596,7 +1183,8 @@ public:
 
         switch (state) {
 
-        
+        case 0:
+    return checked_event_forwarding(handler_0.Int64(i));
 
         default:
             break;
@@ -611,7 +1199,8 @@ public:
 
         switch (state) {
 
-        
+        case 0:
+    return checked_event_forwarding(handler_0.Uint64(i));
 
         default:
             break;
@@ -626,7 +1215,8 @@ public:
 
         switch (state) {
 
-        
+        case 0:
+    return checked_event_forwarding(handler_0.Double(d));
 
         default:
             break;
@@ -641,7 +1231,8 @@ public:
 
         switch (state) {
 
-        
+        case 0:
+    return checked_event_forwarding(handler_0.String(str, length, copy));
 
         default:
             break;
@@ -657,7 +1248,8 @@ public:
         if (depth == 1) {
             if (0) {
             }
-            
+            else if (utility::string_equal(str, length, "\x48\x6f\x72\x69\x7a\x6f\x6e\x74\x61\x6c\x20\x41\x6e\x67\x6c\x65", 16))
+						 { state=0;  }
             else {
                 state = -1;
                 return true;
@@ -666,7 +1258,8 @@ public:
         } else {
             switch (state) {
 
-            
+            case 0:
+    return checked_event_forwarding(handler_0.Key(str, length, copy));
 
             default:
                 break;
@@ -682,7 +1275,8 @@ public:
 
         switch (state) {
 
-        
+        case 0:
+    return checked_event_forwarding(handler_0.StartArray());
 
         default:
             break;
@@ -697,7 +1291,8 @@ public:
 
         switch (state) {
 
-        
+        case 0:
+    return checked_event_forwarding(handler_0.EndArray(length));
 
         default:
             break;
@@ -712,7 +1307,8 @@ public:
 
             switch (state) {
 
-            
+            case 0:
+    return checked_event_forwarding(handler_0.StartObject());
 
             default:
                 break;
@@ -728,7 +1324,8 @@ public:
 
             switch (state) {
 
-            
+            case 0:
+    return checked_event_forwarding(handler_0.EndObject(length));
 
             default:
                 break;
@@ -753,7 +1350,8 @@ public:
 
         switch (state) {
 
-        
+        case 0:
+     handler_0.ReapError(errs); break;
 
         default:
             break;
@@ -768,7 +1366,8 @@ public:
         state = -1;
         the_error.reset();
         reset_flags();
-        
+        handler_0.PrepareForReuse();
+
     }
 };
 
@@ -779,9 +1378,9 @@ struct Serializer< Writera6a4a08c0852b3074f98af3855ef76af843d91d54f7bab9b91bb165
     {
         w.StartObject();
 
-        
+        w.Key("\x48\x6f\x72\x69\x7a\x6f\x6e\x74\x61\x6c\x20\x41\x6e\x67\x6c\x65", 16, false); Serializer< Writera6a4a08c0852b3074f98af3855ef76af843d91d54f7bab9b91bb1658487a34ad, sz::Multi_Brain::Horizontal_Angle >()(w, value.horizontal_angle);
 
-        w.EndObject(0);
+        w.EndObject(1);
     }
 
 };
