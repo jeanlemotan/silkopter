8b49299ddef92baa2fe7f20870e853ab7cc323f3
jean <catalin.vasile@gmail.com>
2015-08-30 20:15:52 +0200

    Removed the unneded ECEF Acceleration. The Linear acceleration is the one used

--- silkopter/libs/common/stream/IAcceleration.h
+++ silkopter/libs/common/stream/IAcceleration.h
@@ -29,16 +29,16 @@ public:
 };
 DECLARE_CLASS_PTR(IENU_Acceleration);
 
-class IECEF_Acceleration : public ISpatial_Stream<Type::ACCELERATION, Space::ECEF>
-{
-public:
-    typedef std::true_type can_be_filtered_t;
+//class IECEF_Acceleration : public ISpatial_Stream<Type::ACCELERATION, Space::ECEF>
+//{
+//public:
+//    typedef std::true_type can_be_filtered_t;
 
-    typedef math::vec3f             Value; //meters per second^2
-    typedef stream::Sample<Value>     Sample;
-    virtual auto get_samples() const -> std::vector<Sample> const& = 0;
-};
-DECLARE_CLASS_PTR(IECEF_Acceleration);
+//    typedef math::vec3f             Value; //meters per second^2
+//    typedef stream::Sample<Value>     Sample;
+//    virtual auto get_samples() const -> std::vector<Sample> const& = 0;
+//};
+//DECLARE_CLASS_PTR(IECEF_Acceleration);
 
 }
 }
