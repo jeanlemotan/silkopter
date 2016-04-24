cb9ff84cc159810b7a89deb001e2cc559707c936
jeanleflambeur <catalin.vasile@gmail.com>
2016-04-25 00:37:01 +0200

    fixed inheritance error

--- def_lang/include/def_lang/Type_System.h
+++ def_lang/include/def_lang/Type_System.h
@@ -10,19 +10,13 @@ namespace ts
 class ITemplate_Argument;
 class ITemplated_Type;
 
-class Type_System final : virtual public Declaration_Scope_EP
+class Type_System final : public Declaration_Scope_EP
 {
 public:
     Type_System();
     Result<std::shared_ptr<const ITemplated_Type>> instantiate_template(std::string const& name, std::vector<std::shared_ptr<const ITemplate_Argument>> const& arguments);
 
     void populate_builtin_types();
-
-    using Declaration_Scope_EP::add_symbol;
-    using Declaration_Scope_EP::get_symbol_count;
-    using Declaration_Scope_EP::get_symbol;
-    using Declaration_Scope_EP::get_symbol;
-
 };
 
 }
