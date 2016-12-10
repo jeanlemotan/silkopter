3f5dc3dc32a9b59dd6fd29fd4e9a90175cca5ce7
jeanleflambeur <catalin.vasile@gmail.com>
2016-12-10 15:18:04 +0100

    RFM22B driver

---
+++ silkopter/libs/utils/hw/RFM22B.cpp
@@ -0,0 +1,514 @@
+#include "RFM22B.h"
+#include "SPI_Dev.h"
+
+#include <thread>
+
+#ifdef RASPBERRY_PI
+extern "C"
+{
+    #include "pigpio.h"
+}
+extern std::chrono::microseconds PIGPIO_PERIOD;
+
+#endif
+
+namespace util
+{
+namespace hw
+{
+
+constexpr size_t MAX_PACHET_SIZE = 64;
+
+/////////////////////////////////////////////////////////////////
+
+RFM22B::RFM22B()
+{
+
+}
+
+RFM22B::~RFM22B()
+{
+
+}
+
+bool RFM22B::init(std::string const& device, uint32_t speed, uint8_t sdn_gpio, uint8_t nirq_gpio)
+{
+    if (m_is_initialized)
+    {
+        return true;
+    }
+
+    auto result = m_spi_dev.init(device, speed);
+    if (result != ts::success)
+    {
+        QLOGE("SPI initialization failed: {}", result.error().what());
+        return false;
+    }
+
+    m_sdn_gpio = sdn_gpio;
+    m_nirq_gpio = nirq_gpio;
+
+    //configure the GPIOs
+
+    int sdn_gpio_mode = gpioGetMode(m_sdn_gpio);
+    if (sdn_gpio_mode == PI_BAD_GPIO)
+    {
+        QLOGE("The SDN GPIO {} is bad", m_sdn_gpio);
+        return false;
+    }
+    int nirq_gpio_mode = gpioGetMode(m_nirq_gpio);
+    if (nirq_gpio_mode == PI_BAD_GPIO)
+    {
+        QLOGE("The nIRQ GPIO {} is bad", m_nirq_gpio);
+        return false;
+    }
+
+    int res = gpioSetMode(m_sdn_gpio, PI_OUTPUT);
+    if (res != 0)
+    {
+        QLOGE("The SDN GPIO {} is bad", m_sdn_gpio);
+        goto error;
+    }
+    res = gpioSetMode(m_nirq_gpio, PI_INPUT);
+    if (res != 0)
+    {
+        QLOGE("The nIRQ GPIO {} is bad", m_nirq_gpio);
+        goto error;
+    }
+
+    shutdown();
+    powerup();
+
+    // Software reset the device
+    reset();
+    idle_mode();
+
+    // Get the device type and check it
+    // This also tests whether we are really connected to a device
+    {
+        uint8_t device_type = get_register(Register::DEVICE_TYPE_00);
+        if (device_type != (uint8_t)Device_Type::RX_TRX && device_type != (uint8_t)Device_Type::TX)
+        {
+            QLOGE("Wrong device type: {}", device_type);
+            goto error;
+        }
+    }
+
+    set_gpio_function(RFM22B::GPIO::GPIO0, RFM22B::GPIO_Function::TX_STATE);
+    set_gpio_function(RFM22B::GPIO::GPIO1, RFM22B::GPIO_Function::RX_STATE);
+
+    m_is_initialized = true; //set to true so we can call call_api
+
+    return true;
+
+error:
+    shutdown();
+
+    m_is_initialized = false;
+
+    gpioSetMode(m_sdn_gpio, sdn_gpio_mode);
+    gpioSetMode(m_nirq_gpio, nirq_gpio_mode);
+    return false;
+}
+
+bool RFM22B::shutdown()
+{
+    if (!m_is_initialized)
+    {
+        return false;
+    }
+    int res = gpioWrite(m_sdn_gpio, 1);
+    if (res != 0)
+    {
+        QLOGE("Failed to shutdown");
+        return false;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(7));
+    return true;
+}
+
+bool RFM22B::powerup()
+{
+    if (!m_is_initialized)
+    {
+        return false;
+    }
+
+    int res = gpioWrite(m_sdn_gpio, 0);
+    if (res != 0)
+    {
+        QLOGE("Failed to powerup");
+        return false;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    return true;
+}
+
+void RFM22B::reset()
+{
+    set_operating_mode((uint16_t)Operating_Mode::READY_MODE | (uint16_t)Operating_Mode::RESET);
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+}
+
+
+void RFM22B::set_modem_configuration(uint8_t data[42])
+{
+    uint8_t const* ptr = data;
+    set_register(Register::IF_FILTER_BANDWIDTH_1C, *ptr++);
+    set_register(Register::AFC_LOOP_GEARSHIFT_OVERRIDE_1D, *ptr++);
+
+    set_register(Register::CLOCK_RECOVERY_OVERSAMPLING_RATIO_20, *ptr++);
+    set_register(Register::CLOCK_RECOVERY_OFFSET_2_21, *ptr++);
+    set_register(Register::CLOCK_RECOVERY_OFFSET_1_22, *ptr++);
+    set_register(Register::CLOCK_RECOVERY_OFFSET_0_23, *ptr++);
+
+    set_register(Register::CLOCK_RECOVERT_TIMING_LOOP_GAIN_1_24, *ptr++);
+    set_register(Register::CLOCK_RECOVERT_TIMING_LOOP_GAIN_0_25, *ptr++);
+
+    set_register(Register::AFC_LIMITER_2A, *ptr++);
+    set_register(Register::OOK_COUNTER_VALUE_1_2C, *ptr++);
+    set_register(Register::OOK_COUNTER_VALUE_2_2D, *ptr++);
+    set_register(Register::SLICER_PEAK_HOLD_2E, *ptr++);
+
+    set_register(Register::DATA_ACCESS_CONTROL_30, *ptr++);
+    set_register(Register::HEADER_CONTROL_1_32, *ptr++);
+    set_register(Register::HEADER_CONTROL_2_33, *ptr++);
+    set_register(Register::PREAMBLE_LENGTH_34, *ptr++);
+    set_register(Register::PREAMBLE_DETECTION_CONTROL_35, *ptr++);
+    set_register(Register::SYNC_WORD_3_36, *ptr++);
+    set_register(Register::SYNC_WORD_2_37, *ptr++);
+    set_register(Register::SYNC_WORD_1_38, *ptr++);
+    set_register(Register::SYNC_WORD_0_39, *ptr++);
+
+    set_register(Register::TRANSMIT_HEADER_3_3A, *ptr++);
+    set_register(Register::TRANSMIT_HEADER_2_3B, *ptr++);
+    set_register(Register::TRANSMIT_HEADER_1_3C, *ptr++);
+    set_register(Register::TRANSMIT_HEADER_0_3D, *ptr++);
+    set_register(Register::TRANSMIT_PACKET_LENGTH_3E, *ptr++);
+    set_register(Register::CHECK_HEADER_3_3F, *ptr++);
+    set_register(Register::CHECK_HEADER_2_40, *ptr++);
+    set_register(Register::CHECK_HEADER_1_41, *ptr++);
+    set_register(Register::CHECK_HEADER_0_42, *ptr++);
+    set_register(Register::HEADER_ENABLE_3_43, *ptr++);
+    set_register(Register::HEADER_ENABLE_2_44, *ptr++);
+    set_register(Register::HEADER_ENABLE_1_45, *ptr++);
+    set_register(Register::HEADER_ENABLE_0_46, *ptr++);
+
+    set_register(Register::TX_DATA_RATE_1_6E, *ptr++);
+    set_register(Register::TX_DATA_RATE_0_6F, *ptr++);
+
+    set_register(Register::MODULATION_MODE_CONTROL_1_70, *ptr++);
+    set_register(Register::MODULATION_MODE_CONTROL_2_71, *ptr++);
+    set_register(Register::FREQUENCY_DEVIATION_72, *ptr++);
+
+    set_register(Register::FREQUENCY_BAND_SELECT_75, *ptr++);
+    set_register(Register::NOMINAL_CARRIER_FREQUENCY_1_76, *ptr++);
+    set_register(Register::NOMINAL_CARRIER_FREQUENCY_0_77, *ptr++);
+}
+
+void RFM22B::idle_mode()
+{
+    set_operating_mode((uint16_t)Operating_Mode::READY_MODE);
+}
+
+void RFM22B::sleep_mode()
+{
+    set_operating_mode((uint16_t)Operating_Mode::ENABLE_WAKE_UP_TIMER);
+}
+
+void RFM22B::stand_by_mode()
+{
+    get_register(Register::INTERRUPT_STATUS_1_03);
+    get_register(Register::INTERRUPT_STATUS_2_04);
+    set_operating_mode(0);
+}
+
+// Set the operating mode
+//	This function does not toggle individual pins as with other functions
+//	It expects a bitwise-ORed combination of the modes you want set
+void RFM22B::set_operating_mode(uint16_t mode)
+{
+    set_register16(Register::OPERATING_MODE_AND_FUNCTION_CONTROL_1_07, mode);
+}
+
+// Get operating mode (bitwise-ORed)
+uint16_t RFM22B::get_operating_mode() const
+{
+    return get_register16(Register::OPERATING_MODE_AND_FUNCTION_CONTROL_1_07);
+}
+
+// Manuall enter RX or TX mode
+void RFM22B::rx_mode()
+{
+    set_operating_mode((uint16_t)Operating_Mode::READY_MODE | (uint16_t)Operating_Mode::RX_MODE);
+}
+void RFM22B::tx_mode()
+{
+    set_operating_mode((uint16_t)Operating_Mode::READY_MODE | (uint16_t)Operating_Mode::TX_MODE);
+}
+
+// Set or get the GPIO configuration
+void RFM22B::set_gpio_function(GPIO gpio, GPIO_Function func)
+{
+    // Get the GPIO register
+    uint8_t gpioX = get_register((Register)gpio);
+
+    // Clear gpioX bits
+    gpioX &= ~((1<<5)-1);
+
+    // Set the gpioX bits
+    gpioX |= (uint8_t)func;
+
+    // Set the register
+    set_register((Register)gpio, gpioX);
+}
+
+// Set or get the transmission power
+void RFM22B::set_tx_power_dBm(uint8_t power)
+{
+    // Saturate to maximum power
+    if (power > 20)
+    {
+        power = 20;
+    }
+
+    // Get the TX power register
+    uint8_t txp = get_register(Register::TX_POWER_6D);
+
+    // Clear txpow bits
+    txp &= ~(0x07);
+
+    // Calculate txpow bits (See Section 5.7.1 of datasheet)
+    uint8_t txpow = (power + 1) / 3;
+
+    // Set txpow bits
+    txp |= txpow;
+
+    // Set the register
+    set_register(Register::TX_POWER_6D, txp);
+}
+uint8_t RFM22B::get_tx_power_dBm()
+{
+    // Get the TX power register
+    uint8_t txp = get_register(Register::TX_POWER_6D);
+
+    // Get the txpow bits
+    uint8_t txpow = txp & 0x07;
+
+    // Calculate power (see Section 5.7.1 of datasheet)
+    if (txpow == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return txpow * 3 - 1;
+    }
+}
+
+// Get and set the frequency hopping channel
+void RFM22B::set_channel(uint8_t channel)
+{
+    set_register(Register::FREQUENCY_HOPPING_CHANNEL_SELECT_79, channel);
+}
+
+uint8_t RFM22B::get_channel() const
+{
+    return get_register(Register::FREQUENCY_HOPPING_CHANNEL_SELECT_79);
+}
+
+// Get input power (in dBm)
+//	Coefficients approximated from the graph in Section 8.10 of the datasheet
+int8_t RFM22B::get_input_dBm() const
+{
+    return 0.56*get_register(Register::RECEIVED_SIGNAL_STRENGTH_INDICATOR_26)-128.8;
+}
+
+bool RFM22B::get_nirq_level()
+{
+    QASSERT(m_is_initialized);
+    return gpioRead(m_nirq_gpio) != 0;
+}
+
+bool RFM22B::write_tx_fifo(void const* data, size_t size)
+{
+    if (!m_is_initialized)
+    {
+        return false;
+    }
+
+    //Toggle ffclrtx bit high and low to clear TX FIFO
+    set_register(Register::OPERATING_MODE_AND_FUNCTION_CONTROL_2_08, 1);
+    set_register(Register::OPERATING_MODE_AND_FUNCTION_CONTROL_2_08, 0);
+
+    if (data == nullptr || size == 0 || size > MAX_PACHET_SIZE)
+    {
+        QASSERT(false);
+        return false;
+    }
+
+    uint8_t buffer[MAX_PACHET_SIZE + 1];
+    buffer[0] = (uint8_t)Register::FIFO_ACCESS_7F | (1<<7);
+
+    memcpy(buffer + 1, data, size);
+
+    return m_spi_dev.transfer(buffer, nullptr, size + 1);
+}
+
+bool RFM22B::read_rx_fifo(void* data, size_t size)
+{
+    if (!m_is_initialized)
+    {
+        return false;
+    }
+
+    if (data == nullptr || size == 0 || size > 64)
+    {
+        QASSERT(false);
+        return false;
+    }
+
+    uint8_t buffer[MAX_PACHET_SIZE + 1] = { 0xFF };
+    buffer[0] = (uint8_t)Register::FIFO_ACCESS_7F;
+
+    if (!m_spi_dev.transfer(buffer, buffer, size + 1))
+    {
+        return false;
+    }
+
+    memcpy(data, buffer + 1, size);
+
+    return true;
+}
+
+// Helper function to read a single byte from the device
+uint8_t RFM22B::get_register(Register reg) const
+{
+    // rx and tx arrays must be the same length
+    // Must be 2 elements as the device only responds whilst it is being sent
+    // data. tx[0] should be set to the requested register value and tx[1] left
+    // clear. Once complete, rx[0] will be left clear (no data was returned whilst
+    // the requested register was being sent), and rx[1] will contain the value
+    uint8_t tx_data[2] = {uint8_t(reg), 0x00};
+    uint8_t rx_data[2] = { 0 };
+    if (!m_spi_dev.transfer(tx_data, rx_data, 2))
+    {
+        QLOGE("Failed to transfer SPI data");
+        return 0;
+    }
+    return rx_data[1];
+}
+
+// Similar to function above, but for readying 2 consequtive registers as one
+uint16_t RFM22B::get_register16(Register reg) const
+{
+    uint8_t tx_data[3] = {uint8_t(reg), 0x00, 0x00};
+    uint8_t rx_data[3] = { 0 };
+    if (!m_spi_dev.transfer(tx_data, rx_data, 3))
+    {
+        QLOGE("Failed to transfer SPI data");
+        return 0;
+    }
+    return (rx_data[1] << 8) | rx_data[2];
+}
+
+// Helper function to write a single byte to a register
+void RFM22B::set_register(Register reg, uint8_t value)
+{
+    uint8_t buffer[] = {uint8_t(uint8_t(reg) | (1 << 7)), value};
+    if (!m_spi_dev.transfer(buffer, nullptr, 2))
+    {
+        QLOGE("Failed to transfer SPI data");
+    }
+}
+
+// As above, but for 2 consequitive registers
+void RFM22B::set_register16(Register reg, uint16_t value)
+{
+    uint8_t buffer[] = {uint8_t(uint8_t(reg) | (1 << 7)), uint8_t(value >> 8), uint8_t(value & 0xFF)};
+    if (!m_spi_dev.transfer(buffer, nullptr, 3))
+    {
+        QLOGE("Failed to transfer SPI data");
+    }
+}
+
+bool RFM22B::tx(size_t size, uint8_t channel)
+{
+    set_register(Register::TRANSMIT_PACKET_LENGTH_3E, size);
+
+    tx_mode();
+
+    // Timing for the interrupt loop timeout
+    auto start = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::high_resolution_clock::now() - start;
+    std::chrono::milliseconds timeout(100);
+
+    uint32_t count = 0;
+
+    // Loop until packet has been sent (device has left TX mode)
+    while (((get_register(Register::OPERATING_MODE_AND_FUNCTION_CONTROL_1_07)>>3) & 1) && elapsed < timeout)
+    {
+        count++;
+        elapsed = std::chrono::high_resolution_clock::now() - start;
+    }
+
+    // If timeout occured, return -1
+    if (elapsed >= timeout)
+    {
+        QLOGW("Timeout while sending: {}", count);
+        //reset();
+        idle_mode();
+        return false;
+    }
+
+    return true;
+}
+bool RFM22B::rx(size_t& size, uint8_t channel, std::chrono::high_resolution_clock::duration timeout)
+{
+    //Toggle ffclrrx bit high and low to clear RX FIFO
+    set_register(Register::OPERATING_MODE_AND_FUNCTION_CONTROL_2_08, 2);
+    set_register(Register::OPERATING_MODE_AND_FUNCTION_CONTROL_2_08, 0);
+
+    rx_mode();
+
+    auto start = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::high_resolution_clock::now() - start;
+
+    uint32_t count = 0;
+
+    // Loop endlessly on interrupt or timeout
+    //	Don't use interrupt registers here as these don't seem to behave consistently
+    //	Watch the operating mode register for the device leaving RX mode. This is indicitive
+    //	of a valid packet being received
+    while (((get_register(Register::OPERATING_MODE_AND_FUNCTION_CONTROL_1_07)>>2) & 1) && elapsed < timeout)
+    {
+        count++;
+        elapsed = std::chrono::high_resolution_clock::now() - start;
+    }
+
+    // If timeout occured, return -1
+    if (elapsed >= timeout)
+    {
+        size = 0;
+        return true; //no error
+    }
+
+    uint8_t is = get_register(Register::INTERRUPT_STATUS_1_03);
+    if ((is & 0x2) == 0)
+    {
+        size = 0;
+        return true;
+    }
+
+    size = get_register(Register::RECEIVED_PACKET_LENGTH_4B);
+    if (size > 64)
+    {
+        size = 0;
+    }
+
+    return true;
+}
+
+
+}
+}
---
+++ silkopter/libs/utils/hw/RFM22B.h
@@ -0,0 +1,399 @@
+#pragma once
+
+#include <string>
+#include "SPI_Dev.h"
+
+namespace util
+{
+namespace hw
+{
+
+class RFM22B
+{
+public:
+    RFM22B();
+    ~RFM22B();
+
+    enum class Register : uint8_t
+    {
+        DEVICE_TYPE_00 								= 0x00,
+        DEVICE_VERSION_01							= 0x01,
+        DEVICE_STATUS_02							= 0x02,
+        INTERRUPT_STATUS_1_03						= 0x03,
+        INTERRUPT_STATUS_2_04						= 0x04,
+        INTERRUPT_ENABLE_1_05						= 0x05,
+        INTERRUPT_ENABLE_2_06						= 0x06,
+        OPERATING_MODE_AND_FUNCTION_CONTROL_1_07	= 0x07,
+        OPERATING_MODE_AND_FUNCTION_CONTROL_2_08	= 0x08,
+        CRYSTAL_OSCILLATOR_LOAD_CAPACITANCE_09		= 0x09,
+        MICROCONTROLLER_OUTPUT_CLOCK_0A				= 0x0A,
+        GPIO0_CONFIGURATION_0B						= 0x0B,
+        GPIO1_CONFIGURATION_0C						= 0x0C,
+        GPIO2_CONFIGURATION_0D						= 0x0D,
+        IO_PORT_CONFIGURATION_0E					= 0x0E,
+        ADC_CONFIGURATION_0F						= 0x0F,
+        ADC_SENSOR_AMPLIFIER_OFFSET_10				= 0x10,
+        ADC_VALUE_11								= 0x11,
+        TEMPERATURE_SENSOR_CONTROL_12				= 0x12,
+        TEMPERATURE_VALUE_OFFSET_13					= 0x13,
+        WAKE_UP_TIMER_PERIOD_1_14					= 0x14,
+        WAKE_UP_TIMER_PERIOD_2_15					= 0x15,
+        WAKE_UP_TIMER_PERIOD_3_16					= 0x16,
+        WAKE_UP_TIMER_VALUE_1_17					= 0x17,
+        WAKE_UP_TIMER_VALUE_2_18					= 0x18,
+        LOW_DUTY_CYCLE_MODE_DURATION_19				= 0x19,
+        LOW_BATTERY_DETECTOR_THRESHOLD_1A			= 0x1A,
+        BATTERY_VOLTAGE_LEVEL_1B					= 0x1B,
+        IF_FILTER_BANDWIDTH_1C						= 0x1C,
+        AFC_LOOP_GEARSHIFT_OVERRIDE_1D				= 0x1D,
+        AFC_TIMING_CONTROL_1E						= 0x1E,
+        CLOCK_RECOVERY_GEARSHIFT_OVERRIDE_1F		= 0x1F,
+        CLOCK_RECOVERY_OVERSAMPLING_RATIO_20		= 0x20,
+        CLOCK_RECOVERY_OFFSET_2_21					= 0x21,
+        CLOCK_RECOVERY_OFFSET_1_22					= 0x22,
+        CLOCK_RECOVERY_OFFSET_0_23					= 0x23,
+        CLOCK_RECOVERT_TIMING_LOOP_GAIN_1_24		= 0x24,
+        CLOCK_RECOVERT_TIMING_LOOP_GAIN_0_25		= 0x25,
+        RECEIVED_SIGNAL_STRENGTH_INDICATOR_26		= 0x26,
+        RSSI_THRESHOLD_FOR_CLEAR_CHANNEL_INDICATOR_27	= 0x27,
+        ANTENNA_DIVERSITY_REGISTER_1_28				= 0x28,
+        ANTENNA_DIVERSITY_REGISTER_2_29				= 0x29,
+        AFC_LIMITER_2A								= 0x2A,
+        AFC_CORRECTION_READ_2B						= 0x2B,
+        OOK_COUNTER_VALUE_1_2C						= 0x2C,
+        OOK_COUNTER_VALUE_2_2D						= 0x2D,
+        SLICER_PEAK_HOLD_2E							= 0x2E,
+    //	RESERVED_2F 								= 0x2F,
+        DATA_ACCESS_CONTROL_30						= 0x30,
+        EXMAC_STATUS_31								= 0x31,
+        HEADER_CONTROL_1_32							= 0x32,
+        HEADER_CONTROL_2_33							= 0x33,
+        PREAMBLE_LENGTH_34							= 0x34,
+        PREAMBLE_DETECTION_CONTROL_35				= 0x35,
+        SYNC_WORD_3_36								= 0x36,
+        SYNC_WORD_2_37								= 0x37,
+        SYNC_WORD_1_38								= 0x38,
+        SYNC_WORD_0_39								= 0x39,
+        TRANSMIT_HEADER_3_3A						= 0x3A,
+        TRANSMIT_HEADER_2_3B						= 0x3B,
+        TRANSMIT_HEADER_1_3C						= 0x3C,
+        TRANSMIT_HEADER_0_3D						= 0x3D,
+        TRANSMIT_PACKET_LENGTH_3E					= 0x3E,
+        CHECK_HEADER_3_3F							= 0x3F,
+        CHECK_HEADER_2_40							= 0x40,
+        CHECK_HEADER_1_41							= 0x41,
+        CHECK_HEADER_0_42							= 0x42,
+        HEADER_ENABLE_3_43							= 0x43,
+        HEADER_ENABLE_2_44							= 0x44,
+        HEADER_ENABLE_1_45							= 0x45,
+        HEADER_ENABLE_0_46							= 0x46,
+        RECEIVED_HEADER_3_47						= 0x47,
+        RECEIVED_HEADER_2_48						= 0x48,
+        RECEIVED_HEADER_1_49						= 0x49,
+        RECEIVED_HEADER_0_4A						= 0x4A,
+        RECEIVED_PACKET_LENGTH_4B					= 0x4B,
+    //	RESERVED_4C									= 0x4C,
+    //	RESERVED_4D									= 0x4D,
+    //	RESERVED_4E									= 0x4E,
+        ADC8_CONTROL_4F								= 0x4F,
+    //	RESERVED									= 0x50,
+    //	RESERVED									= 0x51,
+    //	RESERVED									= 0x52,
+    //	RESERVED									= 0x53,
+    //	RESERVED									= 0x54,
+    //	RESERVED									= 0x55,
+    //	RESERVED									= 0x56,
+    //	RESERVED									= 0x57,
+    //	RESERVED									= 0x58,
+    //	RESERVED									= 0x59,
+    //	RESERVED									= 0x5A,
+    //	RESERVED									= 0x5B,
+    //	RESERVED									= 0x5C,
+    //	RESERVED									= 0x5D,
+    //	RESERVED									= 0x5E,
+    //	RESERVED									= 0x5F,
+        CHANNEL_FILTER_COEFFICIENT_ADDRESS_60		= 0x60,
+    //	RESERVED									= 0x61,
+        CRYSTAL_OSCILLATOR_CONTROL_TES_62			= 0x62,
+    //	RESERVED									= 0x63,
+    //	RESERVED									= 0x64,
+    //	RESERVED									= 0x65,
+    //	RESERVED									= 0x66,
+    //	RESERVED									= 0x67,
+    //	RESERVED									= 0x68,
+        AGC_OVERRIDE_1_69							= 0x69,
+    //	RESERVED									= 0x6A,
+    //	RESERVED									= 0x6B,
+    //	RESERVED									= 0x6C,
+        TX_POWER_6D									= 0x6D,
+        TX_DATA_RATE_1_6E							= 0x6E,
+        TX_DATA_RATE_0_6F							= 0x6F,
+        MODULATION_MODE_CONTROL_1_70				= 0x70,
+        MODULATION_MODE_CONTROL_2_71				= 0x71,
+        FREQUENCY_DEVIATION_72						= 0x72,
+        FREQUENCY_OFFSET_1_73						= 0x73,
+        FREQUENCY_OFFSET_2_74						= 0x74,
+        FREQUENCY_BAND_SELECT_75					= 0x75,
+        NOMINAL_CARRIER_FREQUENCY_1_76				= 0x76,
+        NOMINAL_CARRIER_FREQUENCY_0_77				= 0x77,
+    //	RESERVED									= 0x78,
+        FREQUENCY_HOPPING_CHANNEL_SELECT_79			= 0x79,
+        FREQUENCY_HOPPING_STEP_SIZE_7A				= 0x7A,
+    //	RESERVED									= 0x7B,
+        TX_FIFO_CONTROL_1_7C						= 0x7C,
+        TX_FIFO_CONTROL_2_7D						= 0x7D,
+        RX_FIFO_CONTROL_7E							= 0x7E,
+        FIFO_ACCESS_7F								= 0x7F
+    };
+
+    enum class Device_Type : uint8_t
+    {
+        RX_TRX                 						= 0x08,
+        TX                     						= 0x07
+    };
+
+    enum class Modulation_Type  : uint8_t
+    {
+        UNMODULATED_CARRIER							= 0x00,
+        OOK											= 0x01,
+        FSK											= 0x02,
+        GFSK										= 0x03
+    };
+
+    enum class Modulation_Data_Source : uint8_t
+    {
+        DIRECT_GPIO									= 0x00,
+        DIRECT_SDI									= 0x01,
+        FIFO										= 0x02,
+        PN9											= 0x03
+    };
+
+    enum class CRC_Mode : uint8_t
+    {
+        CRC_DISABLED,
+        CRC_DATA_ONLY,
+        CRC_NORMAL,
+    };
+
+    enum class CRC_Polynomial : uint8_t
+    {
+        CCITT										= 0x00,
+        CRC16										= 0x01,
+        IEC16										= 0x02,
+        BIACHEVA									= 0x03,
+    };
+    #define BAICHEVA BIACHEVA // Seems like Biacheva is a typo in the data sheet
+
+    enum class Data_Clock_Configuration : uint8_t
+    {
+        NONE										= 0x00,
+        GPIO										= 0x01,
+        SDO											= 0x02,
+        NIRQ										= 0x03
+    };
+
+    enum class GPIO : uint8_t
+    {
+        GPIO0										= 0x0B,
+        GPIO1										= 0x0C,
+        GPIO2										= 0x0D
+    };
+
+    enum class GPIO_Function : uint8_t
+    {
+        POWER_ON_RESET								= 0x00,		// This depends on the GPIO!
+        WAKE_UP_TIMER_1								= 0x01,
+        LOW_BATTERY_DETECT_1						= 0x02,
+        DIRECT_DIGITAL_INPUT						= 0x03,
+        EXTERNAL_INTERRUPT_FALLING					= 0x04,
+        EXTERNAL_INTERRUPT_RISING					= 0x05,
+        EXTERNAL_INTERRUPT_CHANGE					= 0x06,
+        ADC_ANALOGUE_INPUT							= 0x07,
+    //	RESERVED									= 0x08,
+    //	RESERVED									= 0x09,
+        DIRECT_DIGITAL_OUTPUT						= 0x0A,
+    //	RESERVED									= 0x0B,
+    //	RESERVED									= 0x0C,
+    //	RESERVED									= 0x0D,
+        REFERENCE_VOLTAGE							= 0x0E,
+        DATA_CLOCK_OUTPUT							= 0x0F,
+        DATA_INPUT									= 0x10,
+        EXTERNAL_RETRANSMISSION_REQUEST				= 0x11,
+        TX_STATE									= 0x12,
+        TX_FIFO_ALMOST_FULL							= 0x13,
+        RX_DATA										= 0x14,
+        RX_STATE									= 0x15,
+        RX_FIFO_ALMOST_FULL							= 0x16,
+        ANTENNA_1_SWITCH							= 0x17,
+        ANTENNA_2_SWITCH							= 0x18,
+        VALID_PREAMBLE_DETECTED						= 0x19,
+        INVALID_PREAMBLE_DETECTED					= 0x1A,
+        SYNC_WORD_DETECTED							= 0x1B,
+        CLEAR_CHANNEL_ASSESSMENT					= 0x1C,
+        VDD											= 0x1D,
+        GND											= 0x1E
+    };
+
+    // Interrupt Enable spans 2 registers, but for the purpose of this enum they are treated as one (16 bit) register
+    enum class Interrupt : uint16_t
+    {
+        INT_POWER_ON_RESET_INT							= (1 << 0),
+        INT_CHIP_READY									= (1 << 1),
+        INT_LOW_BATTERY_DETECT							= (1 << 2),
+        INT_WAKE_UP_TIMER								= (1 << 3),
+        INT_RSSI										= (1 << 4),
+        INT_INVALID_PREAMBLE							= (1 << 5),
+        INT_VALID_PREAMBLE								= (1 << 6),
+        INT_SYNC_WORD									= (1 << 7),
+        INT_CRC_ERROR									= (1 << 8),
+        INT_VALID_PACKET_RECEIVED						= (1 << 9),
+        INT_PACKET_SENT									= (1 << 10),
+        INT_EXTERNAL									= (1 << 11),
+        INT_RX_FIFO_ALMOST_FULL_INT						= (1 << 12),
+        INT_TX_FIFO_ALMOST_EMPTY_INT					= (1 << 13),
+        INT_TX_FIFO_ALMOST_FULL_INT						= (1 << 14),
+        INT_FIFO_UNDERFLOW_OVERFLOW						= uint16_t(1 << 15)
+    };
+
+    // Currently no support for antenna diversity settings
+    // Treat registers 1 and 2 as a single 16 bit register (as above)
+    enum class Operating_Mode : uint16_t
+    {
+        TX_FIFO_RESET								= (1 << 0),
+        RX_FIFO_RESET								= (1 << 1),
+        LOW_DUTY_CYCLE_MODE							= (1 << 2),
+        AUTOMATIC_TRANSMISSION						= (1 << 3),
+        RX_MULTI_PACKET								= (1 << 4),
+    //	Antenna diversity (bits 5-7) not supported
+        READY_MODE									= (1 << 8),
+        TUNE_MODE									= (1 << 9),
+        RX_MODE										= (1 << 10),
+        TX_MODE										= (1 << 11),
+        CRYSTAL_OSCILLATOR_SELECT					= (1 << 12),
+        ENABLE_WAKE_UP_TIMER						= (1 << 13),
+        ENABLE_LOW_BATTERY_DETECT					= (1 << 14),
+        RESET										= uint16_t(1 << 15)
+    };
+
+    // RH_RF22_REG_02_DEVICE_STATUS                    0x02
+    enum class Device_Status : uint8_t
+    {
+        FFOVL                              = 0x80,
+        FFUNFL                             = 0x40,
+        RXFFEM                             = 0x20,
+        HEADERR                            = 0x10,
+        FREQERR                            = 0x08,
+        LOCKDET                            = 0x04,
+        CPS                                = 0x03,
+        CPS_IDLE                           = 0x00,
+        CPS_RX                             = 0x01,
+        CPS_TX                             = 0x10,
+    };
+
+    // RH_RF22_REG_75_FREQUENCY_BAND_SELECT            0x75
+    enum class Frequency_Band_Select : uint8_t
+    {
+        SBSEL                              = 0x40,
+        HBSEL                              = 0x20,
+        FB                                 = 0x1f,
+    };
+
+
+    bool init(std::string const& device, uint32_t speed, uint8_t sdn_gpio, uint8_t nirq_gpio);
+
+    bool shutdown();
+    bool powerup();
+
+    void reset();
+
+    // Set or get the frequency hopping channel
+    void set_channel(uint8_t channel);
+    uint8_t get_channel() const;
+
+    //sets the whole modem block with settings obtained from
+    //  http://www.hoperf.com/upload/rf/RF22B%2023B%2031B%2042B%2043B%20Register%20Settings_RevB1-v5.xls
+/*
+    1C - IF_FILTER_BANDWIDTH
+    1D - AFC_LOOP_GEARSHIFT_OVERRIDE
+
+    20 - CLOCK_RECOVERY_OVERSAMPLING_RATIO
+    21 - CLOCK_RECOVERY_OFFSET_2
+    22 - CLOCK_RECOVERY_OFFSET_1
+    23 - CLOCK_RECOVERY_OFFSET_0
+    24 - CLOCK_RECOVERT_TIMING_LOOP_GAIN_1
+    25 - CLOCK_RECOVERT_TIMING_LOOP_GAIN_0
+    2A - AFC_LIMITER
+    2C - OOK_COUNTER_VALUE_1
+    2D - OOK_COUNTER_VALUE_2
+    2E - SLICER_PEAK_HOLD
+
+    30 - DATA_ACCESS_CONTROL
+    33 - HEADER_CONTROL_2
+    34 - PREAMBLE_LENGTH
+    35 - PREAMBLE_DETECTION_CONTROL
+    36 - SYNC_WORD_3
+    37 - SYNC_WORD_2
+    38 - SYNC_WORD_1
+    39 - SYNC_WORD_0
+
+    6E - TX_DATA_RATE_1
+    6F - TX_DATA_RATE_0
+
+    70 - MODULATION_MODE_CONTROL_1
+    71 - MODULATION_MODE_CONTROL_2
+    72 - FREQUENCY_DEVIATION
+
+    75 - FREQUENCY_BAND_SELECT
+    76 - NOMINAL_CARRIER_FREQUENCY_1
+    77 - NOMINAL_CARRIER_FREQUENCY_0
+*/
+    void set_modem_configuration(uint8_t data[42]);
+
+    // Set or get the transmission power
+    void set_tx_power_dBm(uint8_t power);
+    uint8_t get_tx_power_dBm();
+
+    int8_t get_input_dBm() const;
+
+    // Set the operating mode
+    //	This function does not toggle individual pins as with other functions
+    //	It expects a bitwise-ORed combination of the modes you want set
+    void set_operating_mode(uint16_t mode);
+
+    // Get operating mode (bitwise-ORed)
+    uint16_t get_operating_mode() const;
+
+    void rx_mode();
+    void tx_mode();
+    void idle_mode();
+    void sleep_mode();
+    void stand_by_mode();
+
+    void set_gpio_function(GPIO gpio, GPIO_Function func);
+
+    void set_register(Register register, uint8_t value);
+    uint8_t get_register(Register register) const;
+
+    void set_register16(Register register, uint16_t value);
+    uint16_t get_register16(Register register) const;
+
+    bool write_tx_fifo(void const* data, size_t size);
+    bool read_rx_fifo(void* data, size_t size);
+
+    bool tx(size_t size, uint8_t channel);
+    bool rx(size_t& size, uint8_t channel, std::chrono::high_resolution_clock::duration timeout);
+
+    bool get_nirq_level();
+
+private:
+
+    uint8_t m_sdn_gpio = 0;
+    uint8_t m_nirq_gpio = 0;
+
+    SPI_Dev m_spi_dev;
+    bool m_is_initialized = false;
+};
+
+
+}
+}
