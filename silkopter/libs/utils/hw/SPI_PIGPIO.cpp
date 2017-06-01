bb5f44687dbfe962b9aca08ecac949be4365e4fb
jeanleflambeur <catalin.vasile@gmail.com>
2017-06-01 23:55:57 +0200

    Added PIGPIO spi device

---
+++ silkopter/libs/utils/hw/SPI_PIGPIO.cpp
@@ -0,0 +1,168 @@
+#include "SPI_PIGPIO.h"
+
+#ifdef RASPBERRY_PI
+extern "C"
+{
+    #include "utils/hw/pigpio.h"
+}
+#endif
+
+namespace util
+{
+namespace hw
+{
+
+SPI_PIGPIO::SPI_PIGPIO()
+{
+}
+
+SPI_PIGPIO::~SPI_PIGPIO()
+{
+    if (m_fd >= 0)
+    {
+        spiClose(m_fd);
+    }
+}
+
+ts::Result<void> SPI_PIGPIO::init(uint32_t port, uint32_t channel, uint32_t speed, uint32_t mode)
+{
+    if (m_fd >= 0)
+    {
+        spiClose(m_fd);
+        m_fd = -1;
+    }
+
+    if (port > 1)
+    {
+        return ts::Error("Only SPI ports 0 (main) & 1 (aux) are allowed");
+    }
+    if (port == 0 && channel > 1)
+    {
+        return ts::Error("For port 0, only SPI channels 0 & 1 are allowed");
+    }
+    if (port == 1 && channel > 2)
+    {
+        return ts::Error("For port 1, only SPI channels 0 & 1 & 2 are allowed");
+    }
+    if (mode > 3)
+    {
+        return ts::Error("Only SPI modes 0 to 3 are allowed");
+    }
+
+    m_channel = channel;
+    m_speed = speed;
+    m_mode = mode;
+
+#ifdef RASPBERRY_PI
+
+    uint32_t flags = 0;
+    if (port == 1)
+    {
+        flags |= 1 << 8;
+    }
+    flags |= mode & 0x3;
+
+    m_fd = spiOpen(channel, speed, flags);
+    if (m_fd < 0)
+    {
+        return ts::Error("Error opening SPI port " + std::to_string(port) +
+                         ", channel " + std::to_string(channel) +
+                         ", speed " + std::to_string(speed) +
+                         ", mode " + std::to_string(mode) +
+                         ": "  + std::to_string(m_fd));
+    }
+
+#endif
+
+    return ts::success;
+}
+
+void SPI_PIGPIO::lock()
+{
+    m_mutex.lock();
+}
+
+bool SPI_PIGPIO::try_lock()
+{
+    return m_mutex.try_lock();
+}
+void SPI_PIGPIO::unlock()
+{
+    m_mutex.unlock();
+}
+
+bool SPI_PIGPIO::do_transfer(void const* tx_data, void* rx_data, size_t size, uint32_t speed)
+{
+    QASSERT(size > 0);
+    if (size == 0)
+    {
+        return false;
+    }
+
+    //std::lock_guard<SPI_PIGPIO> lg(*this);
+
+#ifdef RASPBERRY_PI
+    int result = 0;
+    if (tx_data && rx_data)
+    {
+        result = spiXfer(m_fd, (char*)tx_data, (char*)rx_data, size);
+    }
+    else if (tx_data)
+    {
+        result = spiWrite(m_fd, (char*)tx_data, size);
+    }
+    else if (rx_data)
+    {
+        result = spiRead(m_fd, (char*)rx_data, size);
+    }
+
+    if (result < 0)
+    {
+        return false;
+    }
+
+#endif
+
+    return true;
+}
+
+bool SPI_PIGPIO::transfer(void const* tx_data, void* rx_data, size_t size, uint32_t speed)
+{
+    QLOG_TOPIC("spi_PIGPIO::transfer");
+    return do_transfer(tx_data, rx_data, size, speed);
+}
+
+bool SPI_PIGPIO::transfers(Transfer const* transfers, size_t transfer_count, uint32_t speed)
+{
+    for (size_t i = 0; i < transfer_count; i++)
+    {
+        if (!transfer(transfers[i].tx_data, transfers[i].rx_data, transfers[i].size, speed))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool SPI_PIGPIO::transfer_register(uint8_t reg, void const* tx_data, void* rx_data, size_t size, uint32_t speed)
+{
+    QLOG_TOPIC("spi_PIGPIO::transfer_register");
+
+    //std::lock_guard<SPI_PIGPIO> lg(*this);
+
+    m_tx_buffer.resize(size + 1);
+    m_tx_buffer[0] = reg;
+    std::copy(reinterpret_cast<uint8_t const*>(tx_data), reinterpret_cast<uint8_t const*>(tx_data) + size, m_tx_buffer.begin() + 1);
+
+    m_rx_buffer.resize(size + 1);
+    if (!do_transfer(m_tx_buffer.data(), m_rx_buffer.data(), size + 1, speed))
+    {
+        return false;
+    }
+
+    std::copy(m_rx_buffer.begin() + 1, m_rx_buffer.end(), reinterpret_cast<uint8_t*>(rx_data));
+    return true;
+}
+
+}
+}
---
+++ silkopter/libs/utils/hw/SPI_PIGPIO.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "ISPI.h"
+#include <mutex>
+#include <vector>
+
+namespace util
+{
+namespace hw
+{
+
+class SPI_PIGPIO : public ISPI
+{
+public:
+    SPI_PIGPIO();
+    ~SPI_PIGPIO();
+
+    ts::Result<void> init(uint32_t port, uint32_t channel, uint32_t speed, uint32_t mode);
+
+    void lock() override;
+    bool try_lock() override;
+    void unlock() override;
+
+    bool transfer(void const* tx_data, void* rx_data, size_t size, uint32_t speed = 0) override;
+    bool transfer_register(uint8_t reg, void const* tx_data, void* rx_data, size_t size, uint32_t speed = 0) override;
+
+    bool transfers(Transfer const* transfers, size_t transfer_count, uint32_t speed = 0) override;
+
+private:
+    bool do_transfer(void const* tx_data, void* rx_data, size_t size, uint32_t speed);
+
+    uint32_t m_channel = 0;
+    uint32_t m_speed = 0;
+    uint32_t m_mode = 0;
+    int m_fd = -1;
+
+    mutable std::vector<uint8_t> m_tx_buffer;
+    mutable std::vector<uint8_t> m_rx_buffer;
+
+    mutable std::recursive_mutex m_mutex;
+};
+
+}
+}
